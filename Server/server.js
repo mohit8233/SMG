3daf634245440209e05c264f6a52a9c963f2c728
Mohit Singh <mohitbanna085@gmail.com>
2026-05-29 20:30:34 +0530

    BackendDeployinRender

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  origin: ["http://localhost:5173","https://smg-self.vercel.app", "http://localhost:5174", "http://localhost:3000"],
   credentials: true,
 }));
 
