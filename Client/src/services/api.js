bb4b25b5283ab286a13c7fee7bab9dbc85d97247
Mohit Singh <mohitbanna085@gmail.com>
2026-05-29 10:30:06 +0530

    apiUpadateinFrontend

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smg-lqmc.onrender.com/api" || "https://smg-lqmc.onrender.com/api",
+  baseURL: "https://smg-lqmc.onrender.com/api" || "http://localhost:8000/api",
 });
 
 api.interceptors.request.use((config) => {
